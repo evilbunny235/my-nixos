a16c9f946638c6bd111b1eb91045a3badeecc71e
evilbunny235 <evilbunny235@gmail.com>
2024-03-24 21:42:32 +0100

    Add upower

--- nixos-config/hosts/evilpc/default.nix
+++ nixos-config/hosts/evilpc/default.nix
@@ -47,6 +47,8 @@
     steam.enable = true;
   };
 
+  services.upower.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
