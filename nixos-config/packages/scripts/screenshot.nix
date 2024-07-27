0329e8577d1719e9d155e2db5c29a78b5441964a
evilbunny235 <evilbunny235@gmail.com>
2024-07-27 09:54:07 +0200

    replace swappy with satty

--- nixos-config/packages/scripts/screenshot.nix
+++ nixos-config/packages/scripts/screenshot.nix
@@ -2,25 +2,25 @@
   environment.systemPackages = let
     screenshot_area = pkgs.writeShellApplication {
       name = "screenshot_area";
-      runtimeInputs = with pkgs; [grim slurp swappy];
+      runtimeInputs = with pkgs; [grim slurp satty];
       text = ''
-        grim -g "$(slurp -b 00000055 -c 00000000)" - | swappy -f -
+        grim -g "$(slurp -b 00000055 -c 00000000)" - | satty -f -
       '';
     };
 
     screenshot_focused_window = pkgs.writeShellApplication {
       name = "screenshot_focused_window";
-      runtimeInputs = with pkgs; [jq grim swappy];
+      runtimeInputs = with pkgs; [jq grim satty];
       text = ''
-        grim -g "$(hyprctl -j activewindow | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')" - | swappy -f -
+        grim -g "$(hyprctl -j activewindow | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')" - | satty -f -
       '';
     };
 
     screenshot_focused_monitor = pkgs.writeShellApplication {
       name = "screenshot_focused_monitor";
-      runtimeInputs = with pkgs; [jq grim swappy];
+      runtimeInputs = with pkgs; [jq grim satty];
       text = ''
-        grim -o "$(hyprctl -j monitors | jq -r '.[] | select(.focused) | .name')" - | swappy -f -
+        grim -o "$(hyprctl -j monitors | jq -r '.[] | select(.focused) | .name')" - | satty -f -
       '';
     };
   in [
