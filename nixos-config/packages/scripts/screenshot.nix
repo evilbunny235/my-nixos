b0ab08debe7e892cb6aa7ef2623c624318f6f103
evilbunny235 <evilbunny235@gmail.com>
2024-12-24 23:05:59 +0100

    update screenshot scripts

--- nixos-config/packages/scripts/screenshot.nix
+++ nixos-config/packages/scripts/screenshot.nix
@@ -4,7 +4,7 @@
       name = "screenshot_area";
       runtimeInputs = with pkgs; [grim slurp satty];
       text = ''
-        grim -g "$(slurp -b 00000055 -c 00000000)" - | satty -f -
+        grim -g "$(slurp -b 00000055 -c 00000000)" - | satty -f - --output-filename "$HOME/Pictures/Screenshots/$(date '+%Y%m%d-%H:%M:%S').png"
       '';
     };
 
@@ -12,7 +12,7 @@
       name = "screenshot_focused_window";
       runtimeInputs = with pkgs; [jq grim satty];
       text = ''
-        grim -g "$(hyprctl -j activewindow | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')" - | satty -f -
+        grim -g "$(hyprctl -j activewindow | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')" - | satty -f - --output-filename "$HOME/Pictures/Screenshots/$(date '+%Y%m%d-%H:%M:%S').png"
       '';
     };
 
@@ -20,7 +20,7 @@
       name = "screenshot_focused_monitor";
       runtimeInputs = with pkgs; [jq grim satty];
       text = ''
-        grim -o "$(hyprctl -j monitors | jq -r '.[] | select(.focused) | .name')" - | satty -f -
+        grim -o "$(hyprctl -j monitors | jq -r '.[] | select(.focused) | .name')" - | satty -f - --output-filename "$HOME/Pictures/Screenshots/$(date '+%Y%m%d-%H:%M:%S').png"
       '';
     };
   in [
