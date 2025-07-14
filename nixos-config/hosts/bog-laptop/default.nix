97124bb337055adf72f0649789b9ecca9ebd7e6c
evilbunny235 <evilbunny235@gmail.com>
2025-07-14 08:31:26 +0200

    update laptop and ghostty config

--- nixos-config/hosts/bog-laptop/default.nix
+++ nixos-config/hosts/bog-laptop/default.nix
@@ -24,6 +24,7 @@ in {
       pkgs.heaptrack
       pkgs.jq
       pkgs.keydb
+      pkgs.libreoffice-fresh
       pkgs.tcpdump
       pkgs.tokio-console
       pkgs.vesktop
@@ -32,7 +33,7 @@ in {
   };
 
   nixpkgs.config.permittedInsecurePackages = [
-    "beekeeper-studio-5.2.9"
+    "beekeeper-studio-5.2.12"
   ];
 
   programs = {
