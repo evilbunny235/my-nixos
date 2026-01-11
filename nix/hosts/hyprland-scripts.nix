4d8cb89fd745a031eda3a43ddd8381e869d4e7dd
evilbunny235 <125089670+evilbunny235@users.noreply.github.com>
2026-01-11 08:55:31 +0100

    update hyprland hdr script

--- nix/hosts/hyprland-scripts.nix
+++ nix/hosts/hyprland-scripts.nix
@@ -40,7 +40,7 @@
     runtimeInputs = [pkgs.jq];
     text = ''
       if [ "$1" == "true" ]; then
-        hyprctl keyword monitor DP-1, 2560x1440@120, 0x0,1, vrr, 1, bitdepth, 10, cm, hdr, sdrbrightness, 1.1
+        hyprctl keyword monitor DP-1, 2560x1440@120, 0x0,1, vrr, 1, bitdepth, 10, cm, hdr, sdrbrightness, 1.1, sdrsaturation, 1.2
       else
         hyprctl keyword monitor DP-1, 2560x1440@120, 0x0,1, vrr, 1
       fi
